4a67e66e7d1ad36799c243eb28f203927a98986e
rafael <djigit1220@gmail.com>
2025-04-21 11:36:34 +0400

    dz_3

---
+++ main.py
@@ -0,0 +1,26 @@
+
+---
+
+## 🚫 2. Добавим `.gitignore`
+
+Если ты работаешь с Python, создай файл `.gitignore` с таким содержанием:
+
+```gitignore
+# Python
+__pycache__/
+*.py[cod]
+*.pyo
+*.pyd
+*.egg
+*.egg-info/
+dist/
+build/
+.env
+*.sqlite3
+*.log
+
+# VSCode
+.vscode/
+
+# macOS
+.DS_Store
